68146cecfa5ee12fa3063df4e649e24bc71061cb
Anna <annushkashop@gmail.com>
2019-07-12 19:05:09 +0900

    'Autoload_time'

--- database/seeds/TimesTableSeeder.php
+++ database/seeds/TimesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Carbon\Carbon;
 class TimesTableSeeder extends Seeder
 {
     /**
@@ -11,8 +11,21 @@ class TimesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('times')->insert([
-            'time' => '11:00:00',
-        ]);
+        $dates=[];
+        $from = Carbon::createFromTimeString("09:00:00");
+        $to = Carbon::createFromTimeString("18:00:00");
+          
+        for($date = $from; $date->lte($to); $date->addHour()) {      
+             array_push($dates, $date->format('H-i-s'));      
+        }
+
+        for($i=0; $i<count($dates); $i++){
+             DB::table('times')->insert([
+                'time' =>$dates[$i],      
+            ]);
+        }
+        /*DB::table('times')->insert([
+            'time' => "11:00:00",
+        ]);*/
     }
 }
